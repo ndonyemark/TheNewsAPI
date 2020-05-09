611c222ac89fa864576131112f7f8c1c5221ab6f
Ndonye Mark <ndonyemark@gmail.com>
2020-05-09 18:25:20 +0300

    initialised the application configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import config_options
 
 bootstrap = Bootstrap()
 
-def create_app():
+def create_app(config_name):
 
     app = Flask(__name__)
 
-    bootstrap = Bootstrap()
+    bootstrap.init_app(app)
+    app.config.from_object(config_options[config_name])
 
     return app
