45d0c481d6d85d753c66fd8edee90127b695d2fe
Ndonye Mark <ndonyemark@gmail.com>
2020-05-09 21:02:36 +0300

    added views and requests file contents

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,31 @@
 from . import main
 from flask import render_template, request, redirect
+from ..requests import get_sources, get_articles
 
-@main.route("/")
+@app.route("/")
 def index():
 
-    title="News Api app"
-    return render_template("index.html", title=title)
+    general = get_sources('general')
+    business_source_data = get_sources('business')
+    sports_source_data = get_sources('sports')
+    entertainment_source_data = get_sources('entertainment')
+    title = "The Times"
+    return render_template("index.html", title = title, business_news_sources = business_source_data, sports_news_sources = sports_source_data, general_news_sources = general, entertainment_news_sources = entertainment_source_data)
+
+@app.route("/business")
+def business_articles():
+    business_articles = get_articles("business")
+    print(business_articles)
+    return render_template("business.html", business = business_articles)
+
+@app.route("/sports")
+def sports_articles():
+    sports_articles = get_articles("sports")
+    print(sports_articles)
+    return render_template("sports.html", sports = sports_articles)
+
+@app.route("/entertainment")
+def entertainment_articles():
+    entertainment_articles = get_articles("entertainment")
+    print(entertainment_articles)
+    return render_template("entertainment.html", entertainment = entertainment_articles)
