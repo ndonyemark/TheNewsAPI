b31d816bfec5175e5a69c915238a91e40704bbe7
Ndonye Mark <ndonyemark@gmail.com>
2020-05-09 18:32:21 +0300

    registered the main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -11,4 +11,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     app.config.from_object(config_options[config_name])
 
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
